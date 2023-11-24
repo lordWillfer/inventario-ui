7853a7fdab4dba3925bc0ce3090bc5dd432e76f2
Williams E. Fernández <williamsenrique@gmail.com>
2023-11-23 21:39:20 -0600

    Creando componente base para agregar productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
 
 const routes: Routes = [
   { path: 'productos', component: ProductoListaComponent },
-  { path: '', redirectTo: 'productos', pathMatch: 'full' }
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+  { path: 'agregar-producto', component: AgregarProductoComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoListaComponent
+    ProductoListaComponent,
+    AgregarProductoComponent
   ],
   imports: [
     BrowserModule,
